a20ad67d140d411621fb0cc53b88de54cd1d2ee3
krishnan <krishnank281099@gmail.com>
2024-09-17 21:58:41 +0530

    nav

--- src/components/Imagebanner.js
+++ src/components/Imagebanner.js
@@ -1,37 +1,50 @@
 import React from 'react'
 import '../component-css/imagebanner.css'
 import { PiNotepadLight } from 'react-icons/pi'
-import { Fade, Slide } from 'react-awesome-reveal'
+import { Fade } from 'react-awesome-reveal'
+import businessVideo from '../images/video.mp4';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { SwiperSlide, Swiper } from 'swiper/react';
 const Imagebanner = () => {
     return (
         <div className='image-section'>
-            <div className='image_banner_section'>
-                <div className='container'>
-                    <div className='inner-content'>
-                        <div className='animation-class'>
-                            <Fade direction='up'>
-                                <h2>
-                                    Your small business
-                                    growth advisors
-                                </h2>
-                            </Fade>
+            <Swiper spaceBetween={10} navigation={true} modules={[Navigation]} grabCursor={true}>
+                <SwiperSlide>
+                    <div className='image_banner_section'>
+                        <div className='video-section'>
+                            <video src={businessVideo} autoPlay muted loop false />
                         </div>
-                        <p>
-                            Get help from Alex Moore, a professional business coach with advanced experience on growth and business scaling.
-                        </p>
-                        <div className='buttons'>
-                            <button type='button' className='service shine-effect'>our service</button>
-                            <button type='button' className='book_date shine-effect'>
-                                <span className='book_appoinment'>
-                                    <span className='calender'>
-                                        <PiNotepadLight /></span>
-                                    book a meeting
-                                </span>
-                            </button>
+                        <div className='container'>
+
+                            <div className='inner-content'>
+                                <div className='animation-class'>
+                                    <Fade direction='up'>
+                                        <h2>
+                                            Your small business
+                                            growth advisors
+                                        </h2>
+                                    </Fade>
+                                </div>
+                                <p>
+                                    Get help from Alex Moore, a professional business coach with advanced experience on growth and business scaling.
+                                </p>
+                                <div className='buttons'>
+                                    <button type='button' className='service shine-effect'>our service</button>
+                                    <button type='button' className='book_date shine-effect'>
+                                        <span className='book_appoinment'>
+                                            <span className='calender'>
+                                                <PiNotepadLight /></span>
+                                            book a meeting
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </SwiperSlide>
+            </Swiper>
         </div>
     )
 }
